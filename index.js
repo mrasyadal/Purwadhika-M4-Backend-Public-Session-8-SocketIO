a5744e4ddfd20bf095dd8b1dd9c84e07b98beba6
mrasyadal <m.rasyad.a.l@gmail.com>
2021-10-11 19:34:31 +0700

    .emit and .on function

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const app = require("express")();
-// sama aja dengan
-// const express = require('express');
-// const app = express();
-
+const express = require("express");
+const cors = require("cors");
 // buat sebuah server method HTTP modules
 const http = require("http");
 const server = http.createServer(app);
@@ -10,26 +7,39 @@ const PORT = 8000;
 // import socket.io
 const socketIO = require("socket.io");
 
+const app = express();
 app.use(cors());
+app.use(express.json());
 
 // buat http server dengan mentransformasi http di dalam `server` ke dalam `socketIo`
 const io = socketIO(server);
-let arrMsg = []
+let arrMsg = [];
 app.io = io;
 // menyimpan message
 app.arrMsg = arrMsg;
 
+// menerima data dr FE dan mengirim data (ke DB ??)
+app.post("/sendMessage", (req, res) => {
+	// menerima data dr FE di req.body dan ditampung di variabel `arrMsg`
+	arrMsg.push(req.body);
+
+	// kirim chat terbaru ke sisi FE
+	// note: FE harus menunggu adanya pengiriman dr `emit` dengan event 'chatMessage'
+	io.emit("chatMessage", arr.arrMsg);
+	// kirimkan data chat yg terbaru menggunakan `res`
+	res.status(200).send(arrMsg);
+});
+
 // jalankan io
-io.on("connection", socket => {
-   socket.on('joinChat', (data) => {
-      console.log("User joined: ", data)
-   })
-   socket.on('disconnect', () => {
-      console.log("User disconnected")
-   }
+io.on("connection", (socket) => {
+	socket.on("joinChat", (data) => {
+		console.log("User joined: ", data);
+	});
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
+	});
 });
 
 server.listen(PORT, () => {
 	console.log("Socket server is running at ", PORT);
 });
-
