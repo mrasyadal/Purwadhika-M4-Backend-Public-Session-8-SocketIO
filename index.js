2b6273488d52343dad6ed20e34b31983ef2442ef
mrasyadal <m.rasyad.a.l@gmail.com>
2021-10-11 21:01:37 +0700

    Adding namespaces

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = http.createServer(app);
 const PORT = 8000;
 // import socket.io
 const socketIO = require("socket.io");
+const { channel } = require("diagnostics_channel");
 
 const app = express();
 app.use(cors());
@@ -13,30 +14,55 @@ app.use(express.json());
 
 // buat http server dengan mentransformasi http di dalam `server` ke dalam `socketIo`
 const io = socketIO(server);
+// ada 2 array penyimpan message, dibedakan berdasarkan namespace yg digunakan
 let arrMsg = [];
+let arrChn = [];
 app.io = io;
 // menyimpan message
 app.arrMsg = arrMsg;
 
+// M4S8C3: Emit
 // menerima data dr FE dan mengirim data (ke DB ??)
 app.post("/sendMessage", (req, res) => {
-	// menerima data dr FE di req.body dan ditampung di variabel `arrMsg`
-	arrMsg.push(req.body);
+	// di M4S8C4: pisahkan tempat penyimpanan chat message berdasarkan namespace yg digunakan
+	// membaca nama namespace dari query URL (e.g. /sendMessage?namespace=default)
+	if (req.query.namespace === "default") {
+		// menerima data dr FE di req.body dan ditampung di variabel `arrMsg`
+		arrMsg.push(req.body);
 
-	// kirim chat terbaru ke sisi FE
-	// note: FE harus menunggu adanya pengiriman dr `emit` dengan event 'chatMessage'
-	io.emit("chatMessage", arr.arrMsg);
-	// kirimkan data chat yg terbaru menggunakan `res`
-	res.status(200).send(arrMsg);
+		// kirim chat terbaru ke sisi FE
+		// note: FE harus menunggu adanya pengiriman dr `emit` dengan event 'chatMessage'
+		io.emit("chatMessage", arrMsg);
+		// kirimkan data chat yg terbaru menggunakan `res`
+		res.status(200).send(arrMsg);
+	} else {
+		arrChn.push(req.body);
+
+		channelNsp.emit("chatMessage", arrChn);
+		res.status(200).send(arrChn);
+	}
 });
 
-// jalankan io
+// jalankan io untuk namespace default
 io.on("connection", (socket) => {
 	socket.on("joinChat", (data) => {
-		console.log("User joined: ", data);
+		console.log("User ", data, " joined");
 	});
 	socket.on("disconnect", () => {
-		console.log("User disconnected");
+		console.log("User disconnected from the channel");
+	});
+});
+
+// M4S8C4: Namespace
+// membuat namespace baru
+const channelNsp = io.of("/channel");
+// connect client dgn namespace baru
+channelNsp.on("connection", (socket) => {
+	socket.on("joinChat", (data) => {
+		console.log("User ", data, " joined");
+	});
+	socket.on("disconnect", () => {
+		console.log("User disconnected from the channel");
 	});
 });
 
